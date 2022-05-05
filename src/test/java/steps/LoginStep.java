4b682b272906322c326c27a0d3f5fc0782150317
qaacademy <qaacademy@gmail.com>
2022-05-05 18:26:17 -0300

    alteração versao chromedriver

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -19,7 +19,6 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginStep {
     WebDriver driver;
